0d22121cb119cc37356dcb7bbbab9da931faa142
nazwa269 <nazwafauzia129@gmail.com>
2025-11-24 19:51:44 +0700

    Add files via upload

---
+++ if.js
@@ -0,0 +1,14 @@
+let nilai = "90";
+if (nilai > 80) {
+    document.writeln ("A")
+} else if (nilai > 70) {
+   document.writeln ("B") 
+} else if (nilai > 60) {
+    document.writeln ("C")
+} else {
+document.writeln ("D")
+}
+
+
+
+
